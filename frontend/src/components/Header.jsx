f56e976fca9f3c9981e21ac3ea735a58a9a5dbae
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-26 16:51:11 +0530

    create product form in the modal

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from "react";
 import "../css/dasboard.css";
+import ProductForm from "./ProductForm";
 
 function Header() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
   return (
     <div className="d-flex mt-4 flex-wrap justify-content-between align-items-center">
       <div className="search d-flex align-items-center">
@@ -19,8 +24,38 @@ function Header() {
       </div>
 
       <div className="add-btn mt-3 mt-md-0 me-4">
-        <button className="btn btn-success">Add Products</button>
+        <button className="btn btn-success" onClick={handleShow}>
+          Add Products
+        </button>
       </div>
+
+      <div
+        className={`modal fade ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none" }}
+        tabIndex="-1"
+        role="dialog"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add New Products</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={handleClose}
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <ProductForm onClose={handleClose} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {showModal && (
+        <div className="modal-backdrop fade show" onClick={handleClose}></div>
+      )}
     </div>
   );
 }
--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
-function ProductForm() {
+function ProductForm({onClose}) {
   return (
-    <div>ProductForm</div>
-  )
+    <div>
+      <form>
+        <div className="form-group">
+          <label className="ms-1">Name</label>
+          <input
+            type="text"
+            className="form-control mt-2 mb-3"
+            placeholder="Enter product name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="ms-1">Category</label>
+          <select className="form-select mt-2 mb-3">
+            <option selected>Select category</option>
+            <option>Laptop</option>
+            <option>Mobile Phone</option>
+            <option>Tablet</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label className="ms-1">Quantity</label>
+
+          <div className="d-flex align-items-center mt-2 mb-4">
+            <button className="btn btn-info text-white">+</button>
+            <span className="mx-2">0</span>
+            <button className="btn btn-warning text-white ">-</button>
+          </div>
+        </div>
+
+        <div className="d-flex d-flex justify-content-between">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+
+          <button type="submit" className="btn btn-danger" onClick={onClose}>
+            Cancle
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default ProductForm
+export default ProductForm;
