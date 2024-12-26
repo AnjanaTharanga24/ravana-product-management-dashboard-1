321affe0a3146bfd2e4c1436d2d2afb09b9086b8
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-26 19:36:08 +0530

    create get all products function

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const Product = require('../models/ProductModel');
-const { createProduct } = require('../controllers/ProductController');
+const { createProduct, getAllProducts } = require('../controllers/ProductController');
 const router = express.Router();
 
 router.route("/create").post(createProduct)
+router.route("/").get(getAllProducts)
+
 
 module.exports = router;
 
