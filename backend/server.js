284016f2b79279294d51ef2f410b0a1f482be510
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-26 14:20:03 +0530

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require("dotenv").config();
+const connectDB = require('./config/connectDB');
+
+const app = express();
+
+connectDB();
+const PORT = process.env.PORT || 5001
+app.listen(PORT , () =>{
+    console.log(`Server running on port ${PORT}`)
+})
